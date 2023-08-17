6944264afb560d95fb8ab90158d397c7b180d423
Emanuele Fumagalli <emanuelef@gmail.com>
2023-08-17 07:55:00 +0100

    Adjust k6 load test script

--- k6-load/load.js
+++ k6-load/load.js
@@ -3,16 +3,16 @@ import { check, group, sleep } from "k6";
 
 export const options = {
   stages: [
-    { duration: "20s", target: 20 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: "30s", target: 100 }, // stay at 100 users for 10 minutes
-    { duration: "10s", target: 0 }, // ramp-down to 0 users
+    { duration: "10s", target: 20 }, // simulate ramp-up of traffic from 1 to 20 users over 10 seconds.
+    { duration: "20s", target: 100 }, // stay at 100 users for 20 seconds.
+    { duration: "10s", target: 0 }, // ramp-down to 0 users.
   ],
   thresholds: {
     http_req_duration: ["p(99)<1500"], // 99% of requests must completed below 1.5s
   },
 };
 
-const BASE_URL = "http://localhost:8099/hello-resty";
+const BASE_URL = "http://localhost:8080/hello-resty";
 
 export default () => {
   check(http.get(BASE_URL), {
